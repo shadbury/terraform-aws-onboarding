cc5fb90140457054ea636d78f021593eb870541a
Joel.Hutson <joel.hutson@cmd.com.au>
2022-08-02 13:35:01 +1000

    fixing vault issue

--- test/onboarding/test.tf
+++ test/onboarding/test.tf
@@ -7,6 +7,12 @@ module "onboarding" {
   monitoring_role_name      = local.workspace["monitoring_role_name"]
   root_monitoring_account   = data.aws_caller_identity.current.id
   root_monitoring_role_name = local.workspace["root_monitoring_role_name"]
+  patch_alerting_recepients = "joelshadbolt123@hotmail.com"
+  patch_alerting_sender     = "joelshadbolt123@hotmail.com"
+  budget_limit              = 20
+  vault_name                = "AWS_BACKUP_VAULT"
+  budget_subscriber_email_addresses = "joelshadbolt123@hotmail.com"
+
 }
 
 data "aws_caller_identity" "current" {}
